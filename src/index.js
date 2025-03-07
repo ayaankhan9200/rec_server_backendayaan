e861eda0531af7240b249b6a515c441448f0fa37
Ayaan Khan <ayaankh9200@gmail.com>
2025-03-07 15:47:31 +0530

    adding all backend files to github

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import { recipesRouter } from "./routes/recipes.js";
+import { userRouter } from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://ayaankh9200:ayaankhan9200@clusterayaan.uejwt.mongodb.net/?retryWrites=true&w=majority&appName=Clusterayaan",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3000, () => console.log("Server started"));
